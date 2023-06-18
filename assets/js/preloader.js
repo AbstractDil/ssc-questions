e9882ed82f31a682549dc7730ad904cb06145c4a
AbstractDil <nandysagar@yahoo.com>
2023-06-18 23:07:32 +0530

    updated

---
+++ assets/js/preloader.js
@@ -0,0 +1,6 @@
+
+window.onload = function () {
+  document.getElementById('preLoader').style.transition = "all 2s ease-in-out";
+  document.getElementById('preLoader').style.opacity = "0";
+  document.getElementById('preLoader').style.visibility = "hidden";
+}
